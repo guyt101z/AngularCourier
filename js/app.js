9d9805cb8e0c11316aca12bf308364c81fecc249
Luis Campos <luiscampostorreta@gmail.com>
2014-11-07 17:37:35 -0430

    divided panel tab for each and made custom tag

--- js/app.js
+++ js/app.js
@@ -2,15 +2,48 @@
 var app = angular.module('courier', []);
 
 
-/**
- 	por los momentos, esta pagina deberia simplemente deberia conectar todos los modulos
-*/
+// esta directiva controla el comportamiento de los tabs del panel principal
+// de ser necesario podria colocarla aparte en su propio ".js"
+app.directive('panelTabs', function(){
+	return { 
+		restrict:'E',
+		templateUrl:'pages/panel-tabs.html',
+		controller: function(){
+
+				this.tab = 1;
+
+				this.selectTab = function(setTab){
+					this.tab = setTab;
+				};
+				this.isSelected = function(checkTab){
+					return this.tab === checkTab;
+				};
+
+		},
+		controllerAs: 'panel'
+	};
+
+});
+
+//esta directiva se encarga de desplegar el div principal donde se depliegan todos los demas div
+app.directive('panelViews', function(){
+	return { 
+		restrict:'E',
+		templateUrl:'pages/panel-views.html',
+	};
+
+});
+
+
+
 
 app.controller('courierController', function(){	
 	this.agencies = [];
 });
 
-
+/**
+ 	por los momentos, esta pagina deberia simplemente deberia conectar todos los modulos
+*/
 
 /* Por hacer:
 	- Controladores aparte para cada modulo.
@@ -22,27 +55,6 @@ app.controller('courierController', function(){
 //tarea actual?, tabulacion  (aqui en adelante es area de desarrollo, antes de poner codigo en segmentos separados)
 
 
-	app.directive('panelTabs', function(){
-
-		return { 
-			restrict:'E',
-			templateUrl:'pages/panel-tabs.html',
-			controller: function(){
-
-					this.tab = 1;
-
-					this.selectTab = function(setTab){
-						this.tab = setTab;
-					};
-					this.isSelected = function(checkTab){
-						return this.tab === checkTab;
-					};
-
-			},
-			controllerAs: 'panel'
-		};
-
-	});
 
 
 
