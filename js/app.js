ce6c4dee7068d642dc01d8fb1672142a7eb373b0
Luis Campos <luiscampostorreta@gmail.com>
2014-11-06 22:15:30 -0430

    just before adding app-directive

--- js/app.js
+++ js/app.js
@@ -18,12 +18,61 @@ app.controller('PanelController', function(){
 });
 
 
+app.controller("ReviewController", function(){
+		this.review = {};
+
+		this.addReview = function(product){
+			product.reviews.push(this.review);
+			this.review = {};
+		};
+});
+
+// app.directive('ProductPanels', function(){
+
+// 	return {
+// 		restrict:'E',
+// 		templateUrl:'product-panels.html'
+// 		controller:function(){
+// 			this.tab = 1;
+
+// 			this.selectTab = function(setTab){
+// 				this.tab = setTab;
+// 			};
+// 			this.isSelected = function(checkTab){
+// 				return this.tab === checkTab;
+// 			};
+// 		}
+// 		controllerAs: 'panels'
+// 	};
+
+// });
+
+
+
 var gems =[{
 	name: 'Dodecahedron',
 	price: 2.95,
 	description: 'Bonita piedra bro, trust me...',
 	canPurchase: true,
 	soldOut:true,
+	images: [
+		{
+			full: 'img/do-01-full.jpg'
+		}
+
+	],
+	reviews:[
+		{
+		stars: 5,
+		body: "i love this product!",
+		author: "joe@gmail.com"
+		},
+		{
+		stars: 4,
+		body: "Apesta!",
+		author: "nulo2@gmail.com"
+		}
+		]
 },
 {
 	name: 'Pentagonal Gem',
@@ -31,6 +80,43 @@ var gems =[{
 	description: 'Un pentagono!, whoah! mind blow',
 	canPurchase: false,
 	soldOut:true,
+	images: [
+		{
+			full: 'img/ph-01-full.jpeg'
+		}
+
+	],
+	reviews:[
+		{
+		stars: 5,
+		body: "i love this productoso!",
+		author: "joe@gmail.com"
+		},
+		{
+		stars: 1,
+		body: "meh",
+		author: "nulo2@gmail.com"
+		}
+		]
+},{
+	name: 'Rubiridiom Gem',
+	price: 9.44,
+	description: 'Adventure time!',
+	canPurchase: true,
+	soldOut:false,
+	images: [
+		{
+			full: 'img/ru-01-full.jpeg'
+		}
+
+	],
+	reviews:[
+		{
+		stars: 5,
+		body: "i love this product!",
+		author: "joe@gmail.com"
+		}
+		]
 }
 ]
 
